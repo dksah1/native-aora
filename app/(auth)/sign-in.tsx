95f1a7a7b3bb2e5ba4c325a51cdad005868e0c66
dksah1 <dkboss127126@gmail.com>
2024-09-05 23:17:14 +0545

    added login form

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -6,12 +6,17 @@ import {
   TouchableOpacity,
   Image,
   Alert,
+  ScrollView,
 } from "react-native";
 import React, { useState } from "react";
 import { Icon } from "react-native-elements";
 import { Formik } from "formik";
 import * as Yup from "yup";
 import * as ImagePicker from "expo-image-picker";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../../constants";
+import FormField from "@/components/FormField";
+import CustomButton from "@/components/CustomButton";
 
 const SignInSchema = Yup.object().shape({
   email: Yup.string().email("Invalid Email").required("Email is Required"),
@@ -21,6 +26,12 @@ const SignInSchema = Yup.object().shape({
 });
 
 const SignIn = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
   const [image, setImage] = useState<string | null>(null);
 
   const handleSubmitForm = (values: { email: string; password: string }) => {
@@ -31,64 +42,100 @@ const SignIn = () => {
   };
 
   return (
-    <Formik
-      initialValues={{ email: "", password: "" }}
-      validationSchema={SignInSchema}
-      onSubmit={handleSubmitForm}
-    >
-      {({
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        values,
-        errors,
-        touched,
-      }) => (
-        <View style={styles.container}>
-          {image && <Image source={{ uri: image }} style={styles.image} />}
-
-          <TextInput
-            style={styles.input}
-            placeholder="Enter Your Email"
-            placeholderTextColor="#6c6c6c"
-            value={values.email}
-            onChangeText={handleChange("email")}
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full justify-center min-h-[85vh] px-4 my-6">
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            className="w-[115px] h-[35px]"
           />
-          {errors.email && touched.email && (
-            <Text style={styles.errorText}>{errors.email}</Text>
-          )}
+          <Text className="text-2xl text-white text-semibold mt-10 font-psemibold ">
+            SignIn
+          </Text>
 
-          <View style={styles.passwordContainer}>
-            <TextInput
-              style={styles.input}
-              placeholder="Enter your password"
-              placeholderTextColor="#6c6c6c"
-              value={values.password}
-              onChangeText={handleChange("password")}
-            />
-            <Icon type="font-awesome" name="eye" color="#6c6c6c" />
-          </View>
-          {errors.password && touched.password && (
-            <Text style={styles.errorText}>{errors.password}</Text>
-          )}
-          <Text style={styles.passwordHint}>Must be at least 8 characters</Text>
-
-          <TouchableOpacity
-            style={styles.signUpButton}
-            onPress={() => handleSubmit()}
-          >
-            <Text style={styles.signUpButtonText}>Sign In</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.googleButton}>
-            <Text style={styles.googleButtonText}>Sign In with Google</Text>
-          </TouchableOpacity>
+          <FormField
+            title="email"
+            value={form.email}
+            handleChangeText={(e: any) => setForm({ ...form, email: e })}
+            otherStyles="mt-7  "
+            keyboardType="email-address"
+          />
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(e: any) => setForm({ ...form, password: e })}
+            otherStyles="mt-7  "
+            keyboardType="email-address"
+          />
+          <CustomButton
+            title="Sign In"
+            handlePress={SignIn}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
+          />
         </View>
-      )}
-    </Formik>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
 export default SignIn;
+
+// <Formik
+//   initialValues={{ email: "", password: "" }}
+//   validationSchema={SignInSchema}
+//   onSubmit={handleSubmitForm}
+// >
+//   {({
+//     handleChange,
+//     handleBlur,
+//     handleSubmit,
+//     values,
+//     errors,
+//     touched,
+//   }) => (
+//     <View style={styles.container}>
+//       {image && <Image source={{ uri: image }} style={styles.image} />}
+
+//       <TextInput
+//         style={styles.input}
+//         placeholder="Enter Your Email"
+//         placeholderTextColor="#6c6c6c"
+//         value={values.email}
+//         onChangeText={handleChange("email")}
+//       />
+//       {errors.email && touched.email && (
+//         <Text style={styles.errorText}>{errors.email}</Text>
+//       )}
+
+//       <View style={styles.passwordContainer}>
+//         <TextInput
+//           style={styles.input}
+//           placeholder="Enter your password"
+//           placeholderTextColor="#6c6c6c"
+//           value={values.password}
+//           onChangeText={handleChange("password")}
+//         />
+//         <Icon type="font-awesome" name="eye" color="#6c6c6c" />
+//       </View>
+//       {errors.password && touched.password && (
+//         <Text style={styles.errorText}>{errors.password}</Text>
+//       )}
+//       <Text style={styles.passwordHint}>Must be at least 8 characters</Text>
+
+//       <TouchableOpacity
+//         style={styles.signUpButton}
+//         onPress={() => handleSubmit()}
+//       >
+//         <Text style={styles.signUpButtonText}>Sign In</Text>
+//       </TouchableOpacity>
+//       <TouchableOpacity style={styles.googleButton}>
+//         <Text style={styles.googleButtonText}>Sign In with Google</Text>
+//       </TouchableOpacity>
+//     </View>
+//   )}
+// </Formik>
 
 const styles = StyleSheet.create({
   container: {
