6ef3b28c2d5a529cfd6f9c1d6785f1a9f3ee7560
dksah1 <dkboss127126@gmail.com>
2024-09-07 09:34:25 +0545

    added global context final

---
+++ context/GlobalProvider.ts
@@ -0,0 +1,68 @@
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  ReactNode,
+} from "react";
+import { getCurrentUser } from "../lib/appwrite";
+
+// Define the shape of the context
+interface GlobalContextProps {
+  isLogged: boolean;
+  setIsLogged: React.Dispatch<React.SetStateAction<boolean>>;
+  user: any;
+  setUser: React.Dispatch<React.SetStateAction<any>>;
+  loading: boolean;
+}
+
+// Create the context with a default value
+const GlobalContext = createContext<GlobalContextProps | undefined>(undefined);
+export const useGlobalContext = () => {
+  const context = useContext(GlobalContext);
+  if (!context) {
+    throw new Error("useGlobalContext must be used within a GlobalProvider");
+  }
+  return context;
+};
+
+const GlobalProvider = ({ children }: { children: ReactNode }) => {
+  const [isLogged, setIsLogged] = useState(false);
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getCurrentUser()
+      .then((res: any) => {
+        if (res) {
+          setIsLogged(true);
+          setUser(res);
+        } else {
+          setIsLogged(false);
+          setUser(null);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        isLogged,
+        setIsLogged,
+        user,
+        setUser,
+        loading,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
+
+export default GlobalProvider;
